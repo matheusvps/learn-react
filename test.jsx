6d070e1ea962296b8eed625f6fa99e963f9f6df6
Matheus Passos <vpsmatheus@hotmail.com>
2023-08-25 20:05:45 -0300

    new comments and virtual dom summary

--- test.jsx
+++ test.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+//Selects HTML app with "app" id
 const container = document.getElementById('app');
+
+/* createRoot is from react concurrent. Creates a root in order to display
+react components inside it */
 const root = createRoot(container);
+
+//render something inside root
 root.render(<h1>Hello world</h1>);
