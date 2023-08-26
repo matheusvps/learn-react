185e203c1abda6dc07b1d32e20240e59a3b8e946
Matheus Passos <vpsmatheus@hotmail.com>
2023-08-26 12:31:24 -0300

    organization + some module 2 classes

---
+++ module2/class2.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+// Write code here:
+root.render(<h1>2 + 3</h1>);
